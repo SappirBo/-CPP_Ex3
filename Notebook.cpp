f58aad15e01375d75314118323ae509cea27f484
Sappir Bohbot <92790326+SappirBo@users.noreply.github.com>
2022-03-22 16:17:59 +0200

    SetUp V1.2

---
+++ Notebook.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Notebook.hpp"
+
+void ariel::Notebook::write(int page, int row, int column, Direction,std::string txt){
+
+}
+std::string ariel::Notebook::read(int page, int row, int column,Direction,int length){
+    return "";
+}
+void ariel::Notebook::erase(int page, int row, int column,Direction,int length){
+
+}
+std::string ariel::Notebook::show(int page){
+    return "";
+}
---
+++ Notebook.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+#include "Direction.hpp"
+
+
+namespace ariel
+{
+    class Notebook{
+        private:
+        int pages;
+        int rows;
+        int col;
+
+        public:
+        Notebook(){
+            pages = INT_MAX;
+            rows = INT_MAX;
+            col = 100;
+        }
+
+        void write(int page, int row, int column, Direction,std::string txt);
+        std::string read(int page, int row, int column,Direction,int length);
+        void erase(int page, int row, int column,Direction,int length);
+        std::string show(int page);
+
+    };
+}
+
